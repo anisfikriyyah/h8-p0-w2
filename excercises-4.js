5fde36b0e620aba8c8b0deec973f7a98d929a389
anisfikriyyah <anisfikri3@gmail.com>
2018-07-03 20:44:30 +0700

    excercises 4

---
+++ excercises-4.js
@@ -0,0 +1,47 @@
+var tgl = '23';
+var bln = '12' ;
+var thn = '2009' ;
+
+
+
+switch (bln) {
+    case '1':
+    console.log(tgl + ' ' + 'Januari' + ' ' + thn);
+    break
+    case '2':
+    console.log(tgl + ' ' + 'Februari' + ' ' + thn);
+    break
+    case '3':
+    console.log(tgl + ' ' + 'Maret' + ' ' + thn);
+    break
+    case '4':
+    console.log(tgl + ' ' + 'April' + ' ' + thn);
+    break
+    case '5':
+    console.log(tgl + ' ' + 'Mei' + ' ' + thn);
+    break
+    case '6':
+    console.log(tgl + ' ' + 'Juni' + ' ' + thn);
+    break
+    case '7':
+    console.log(tgl + ' ' + 'Juli' + ' ' + thn);
+    break
+    case '8':
+    console.log(tgl + ' ' + 'Agustus' + ' ' + thn);
+    break
+    case '9':
+    console.log(tgl + ' ' + 'September' + ' ' + thn);
+    break
+    case '10':
+    console.log(tgl + ' ' + 'Oktober' + ' ' + thn);
+    break
+    case '11':
+    console.log(tgl + ' ' + 'November' + ' ' + thn);
+    break
+    case '12':
+    console.log(tgl + ' ' + 'Desember' + ' ' + thn);
+    break
+    default:
+    console.log('Format yang anda Masukkan Salah! Silahkan Cek Kembali ');
+    break
+}
