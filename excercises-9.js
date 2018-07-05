6ac656723a4da0735adf4dcc9c4649fdb72e85e6
anisfikriyyah <anisfikri3@gmail.com>
2018-07-05 14:03:45 +0700

    excercises 9

---
+++ excercises-9.js
@@ -0,0 +1,37 @@
+//======================================================================================================
+//Tugas 1
+
+function shoutOut(){
+    console.log ('Halo Function!')
+}
+console.log(shoutOut())
+
+//------------------------------------------------------------------------------------------------------
+//Tugas 2
+
+function calculateMultiply(){
+    var num1 = 5
+    var num2 = 6   
+
+    var hasilPerkalian = num1*num2
+    return hasilPerkalian
+}
+console.log(calculateMultiply())
+
+//-------------------------------------------------------------------------------------------------------
+//Tugas 3
+
+
+function processSentence(){
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence ='Nama Saya '+ name + ' ,Umur saya ' + age + ' Tahun,Alamat saya di ' + address + ' ,Saya Punya hobby yaitu ' + hobby;
+return fullSentence;
+}
+console.log(processSentence());
+
+
+
